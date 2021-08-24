e124c531761ec3b8796111f7c1886a10c3b3e532
Emanuel <emanuelghub@gmail.com>
2021-08-24 20:01:31 +0400

    judypn: Fix derp

--- dot_judypn.mk
+++ dot_judypn.mk
@@ -34,7 +34,7 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # Device identifiers
 
-PRODUCT_NAME := lineage_judypn
+PRODUCT_NAME := dot_judypn
 PRODUCT_DEVICE := judypn
 PRODUCT_BRAND := lge
 PRODUCT_MANUFACTURER := LGE
