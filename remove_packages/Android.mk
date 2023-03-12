8d3b8b8e028ed7682f79f924534955bf11864b2c
ugh5jhr <fisbw92b7sv73@gmail.com>
2023-03-12 10:24:00 +0000

    add unwanted google app

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,11 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES += GCamGo
+LOCAL_OVERRIDES_PACKAGES += GCamGo talkback AndroidAutoStubPrebuilt arcore GoogleTTS MarkupGoogle 
+LOCAL_OVERRIDES_PACKAGES += RecorderPrebuilt CalculatorGooglePrebuilt CalendarGooglePrebuilt 
+LOCAL_OVERRIDES_PACKAGES += WellbeingPrebuilt EmergencyInfoGoogleNoUi LocationHistoryPrebuilt 
+LOCAL_OVERRIDES_PACKAGES += PrebuiltDeskClockGoogle SetupWizardPrebuilt GooglePrintRecommendationService
+LOCAL_OVERRIDES_PACKAGES += BuiltInPrintService Velvet
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
